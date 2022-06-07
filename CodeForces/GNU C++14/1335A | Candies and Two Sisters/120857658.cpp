de1adcef0c6bf0d6633b86721a44ee72117abc67
Parth Santosh Kabra <84372000+parth-kabra@users.noreply.github.com>
2022-06-07 16:00:29 +0530

    Committing files

---
+++ CodeForces/GNU C++14/1335A | Candies and Two Sisters/120857658.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define int long long
+const int MAX=1e6;
+void solve(){
+  int t;cin>>t;
+  while(t--){
+    int a;cin>>a;
+    cout<<(a-1)/2<<endl;
+  }
+}
+int32_t main(){
+ ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+ #ifndef ONLINE_JUDGE
+     freopen("input.txt", "r", stdin);
+     freopen("output.txt", "w", stdout);
+ #endif
+     solve();
+ return 0;
+}
