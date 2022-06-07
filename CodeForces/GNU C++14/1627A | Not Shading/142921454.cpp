7de00bec6c393ed34c9a478391798296331036b5
Parth Santosh Kabra <84372000+parth-kabra@users.noreply.github.com>
2022-06-07 14:50:41 +0530

    Committing files

---
+++ CodeForces/GNU C++14/1627A | Not Shading/142921454.cpp
@@ -0,0 +1,68 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define endl '\n'
+#define int long long
+#define debug(x...) cerr<<#x<<" = "<<x<<endl;
+void usaco(string filename = ""){
+    if(filename.size() > 0){
+        #ifndef ONLINE_JUDGE
+            freopen((filename + ".in").c_str(), "r", stdin);
+            freopen((filename + ".out").c_str(), "w", stdout);
+        #endif
+    }
+    else{
+        #ifndef ONLINE_JUDGE
+            freopen("input.txt", "r", stdin);
+            freopen("output.txt", "w", stdout);
+            freopen("error.txt", "w", stderr); 
+        #endif
+    }
+}
+void Solve(){
+    bool one = false,two = false;
+    int n , m , r, c;
+    cin >> n >> m >> r >> c;
+    int cnt = 0;
+    for(int i=1; i <= n;i++){
+        for(int j = 1; j <= m;j++){
+            char x;
+            cin >> x;
+            if(x == 'B'){
+                if(i == r || j == c){
+                    two = true;
+                }
+                if(i == r and j == c){
+                    one = true;
+                }
+                cnt++;
+            }
+        }
+    }
+    if(one){
+        cout << 0 << endl;
+    }
+    else if(two){
+        cout << 1 << endl;
+    }
+    else if(cnt){
+        cout << 2 << endl;
+    }
+    else{
+        cout << -1 << endl;
+    }
+}
+int32_t main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+ 
+    usaco();
+    
+    int t;
+    cin >> t;
+    while(t--){
+        Solve();
+ 
+    }    
+ 
+    return 0;
+}
