de348fac04ae7f7abe9c7837a0456e121cc79827
Parth Santosh Kabra <84372000+parth-kabra@users.noreply.github.com>
2022-06-07 15:21:33 +0530

    Committing files

---
+++ CodeForces/GNU C++14/1140D | Minimum Triangulation/129039765.cpp
@@ -0,0 +1,38 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define int long long
+#define all(s) s.begin(),s.end()
+const int imax=2147483647;
+const int imin=-2147483648;
+const int mod=1e9+7;
+const int N=1e5+10;
+const int N1=1e5+10;
+bool prime[N];
+int factorials[N1];
+void seive(){memset(prime,true,sizeof(prime));for(int p=2;p*p<=N;p++){if (prime[p] == true){for(int j=p*p;j<=N;j+=p){prime[j]=false;}}}}
+void factorial(){factorials[0]=factorials[1]=1;for(int i=2;i<N1;i++){factorials[i]=factorials[i-1]*i;}}
+void solve(){
+    int n;
+    cin>>n;
+    //factorial();
+    /*if(n&1){
+        cout<<factorials[n]<<endl;
+        return;
+    }*/
+    int temp = factorials[n];
+    int sum=0;
+    for(int i=2;i<n;i++){
+        sum+=i*(i+1);
+    }
+    cout<<sum<<endl;
+}
+int32_t main(){
+    std::ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    #ifndef ONLINE_JUDGE
+        freopen("input.txt", "r", stdin);
+        freopen("uttar.txt", "w", stdout);
+    #endif
+        solve();
+    return 0;
+}
