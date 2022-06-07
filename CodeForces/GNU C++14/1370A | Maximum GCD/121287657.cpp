b023ad97093cab40b6b8bc444d3d9515e1515493
Parth Santosh Kabra <84372000+parth-kabra@users.noreply.github.com>
2022-06-07 15:59:50 +0530

    Committing files

---
+++ CodeForces/GNU C++14/1370A | Maximum GCD/121287657.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define int long long
+#define endl "\n"
+const int MAX=1e6;
+const int mod=1e9+7;
+void solve(){
+    int t;cin>>t;
+    while(t--){
+        int n;cin>>n;
+        cout<<n/2<<endl;
+    }
+}
+int32_t main(){
+    ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+    #ifndef ONLINE_JUDGE
+        freopen("input.txt", "r", stdin);
+        freopen("output.txt", "w", stdout);
+    #endif
+        solve();
+    return 0;
+}
