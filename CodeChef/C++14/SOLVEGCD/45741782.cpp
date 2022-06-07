d624b3938ad0e50acd33028df3032bbdf71d6191
Parth Santosh Kabra <84372000+parth-kabra@users.noreply.github.com>
2022-06-07 14:24:35 +0530

    Committing files

---
+++ CodeChef/C++14/SOLVEGCD/45741782.cpp
@@ -0,0 +1,20 @@
+/*
+		🅿🅰🆁🆃🅷 🅺🅱🆁🅰
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define str string
+void solve(){
+	int a,b;cin>>a>>b;
+	cout<<__gcd(a,b)<<endl;
+}
+int32_t main(){
+	#ifndef ONLINE_JUDGE
+	    freopen("input.txt", "r", stdin);
+	    freopen("output.txt", "w", stdout);
+	#endif
+	   solve();
+	return 0;
+}
