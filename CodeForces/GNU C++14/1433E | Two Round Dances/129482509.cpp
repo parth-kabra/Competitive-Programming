73af1d4a50a3a4a46b855c4270e560ffcada6d79
Parth Santosh Kabra <84372000+parth-kabra@users.noreply.github.com>
2022-06-07 15:18:40 +0530

    Committing files

---
+++ CodeForces/GNU C++14/1433E | Two Round Dances/129482509.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define int long long
+#define all(s) s.begin(),s.end()
+const int imax=2147483647;
+const int imin=-2147483648;
+const int mod=1e9+7;
+const int N=1e5+10;
+bool prime[N];
+int factorials[N];
+void seive(){memset(prime,true,sizeof(prime));for(int p=2;p*p<=N;p++){if (prime[p] == true){for(int j=p*p;j<=N;j+=p){prime[j]=false;}}}}
+void factorial(){factorials[0]=factorials[1]=1;for(int i=2;i<N;i++){factorials[i]=factorials[i-1]*i;}}
+void solve(){
+    factorial();
+    int n;
+    cin>>n;
+    int temp=factorials[n-1];
+    cout<<temp/(n/2)<<endl;
+}
+int32_t main(){
+    std::ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    #ifndef ONLINE_JUDGE
+        freopen("input.txt", "r", stdin);
+        freopen("uttar.txt", "w", stdout);
+    #endif
+        solve();
+    return 0;
+}
