79f30cdec0f07d33a36f53b986812857641e1c47
Parth Santosh Kabra <84372000+parth-kabra@users.noreply.github.com>
2022-06-07 14:39:33 +0530

    Committing files

---
+++ CodeForces/GNU C++14/922A | Cloning Toys/150042745.cpp
@@ -0,0 +1,68 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+ 
+ 
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+template<typename T> using Set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+template<typename T> using Multiset = tree<T, null_type, less_equal<T>, rb_tree_tag, tree_order_statistics_node_update>;
+ 
+ 
+ 
+#define endl '\n'
+#define int long long
+#define debug(x...) cerr<<#x<<" = "<<x<<endl;
+void usaco(string filename = ""){
+    if(filename.size() > 0){
+        #ifndef ONLINE_JUDGE
+            freopen((filename + ".in").c_str(), "r", stdin);
+            freopen((filename + ".out").c_str(), "w", stdout);
+        #endif
+    }
+    else{
+        #ifndef ONLINE_JUDGE
+            freopen("input.txt", "r", stdin);
+            freopen("output.txt", "w", stdout);
+            freopen("error.txt", "w", stderr); 
+        #endif
+    }
+}
+void Solve(){
+    int a, b;
+    cin >> a >> b;
+    if(b == 0){
+        cout << "No" << endl;
+    }
+ 
+    else if(b == 1){
+        if(a){
+            cout << "No" << endl;
+        }
+        else{
+            cout << "Yes" << endl;
+        }
+    }
+ 
+    else if(a >= b - 1 && (a - b + 1) % 2 == 0){
+        cout << "Yes" << endl;
+    }
+    else{
+        cout << "No" << endl;
+    }
+}
+int32_t main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+ 
+    usaco();
+ 
+    int T = 1;
+    //cin >> T;
+    while(T--){
+        Solve();
+    }
+    return 0;
+ 
+}
