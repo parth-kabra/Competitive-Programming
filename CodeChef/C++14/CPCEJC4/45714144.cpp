680580657222732b3383c9aca090fe589352e8d6
Parth Santosh Kabra <84372000+parth-kabra@users.noreply.github.com>
2022-06-07 14:24:48 +0530

    Committing files

---
+++ CodeChef/C++14/CPCEJC4/45714144.cpp
@@ -0,0 +1,25 @@
+/*
+
+	JAI SHREE RAM!
+	BAJRANGBALI KI JAI!
+
+	AUTHOR - PARTH KABRA
+
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define str string
+void solve(){
+	int a,b;cin>>a>>b;
+	cout<<a+b<<endl;
+}
+int32_t main(){
+	#ifndef ONLINE_JUDGE
+	    freopen("input.txt", "r", stdin);
+	    freopen("output.txt", "w", stdout);
+	#endif
+	   solve();
+	return 0;
+}
