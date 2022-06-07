45b25db26707feb81fd4874a5724a319578b4702
Parth Santosh Kabra <84372000+parth-kabra@users.noreply.github.com>
2022-06-07 14:51:29 +0530

    Committing files

---
+++ CodeForces/GNU C++14/1093C | Mishka and the Last Exam/142681941.cpp
@@ -0,0 +1,54 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define endl '\n'
+#define int long long
+#define debug(x...) cerr<<#x<<" = "<<x<<endl;
+void usaco(string filename = ""){
+    if(filename.size() > 0){
+        #ifndef ONLINE_JUDGE
+            freopen((filename + ".in").c_str(), "r", stdin);
+            freopen((filename + ".out").c_str(), "w", stdout);
+        #endif
+    }
+    else{
+        #ifndef ONLINE_JUDGE
+            freopen("input.txt", "r", stdin);
+            freopen("output.txt", "w", stdout);
+            freopen("error.txt", "w", stderr); 
+        #endif
+    }
+}
+const int N = (2 * 1e5) + 5;
+int dp[N];
+int arr[N];
+void Solve(){
+    int n;
+    cin >> n;
+    for(int i=0; i < n / 2 ;i++){
+        cin >> arr[i];
+    }
+    dp[0] = 0;
+    dp[n-1] = arr[0];
+    for(int i = 1; i < n / 2; i++){
+        dp[i] = max(dp[i-1], arr[i] - dp[n-i]);
+        dp[n-i-1] = arr[i] - dp[i];
+    }
+    for(int i=0; i < n ;i++){
+        cout << dp[i] << " ";
+    }
+    cout << endl;
+}
+int32_t main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+ 
+    usaco();
+ 
+    int t = 1;
+    //cin >> t;
+    while(t--){
+        Solve();
+    }
+ 
+    return 0;
+}
