056048650f204eaa9916540889647dffe95683be
Parth Santosh Kabra <84372000+parth-kabra@users.noreply.github.com>
2022-06-07 14:25:01 +0530

    Committing files

---
+++ CodeChef/C++14/HNH01/45539098.cpp
@@ -0,0 +1,30 @@
+/*
+		 ____                   _     _         _  __          _                    
+		|  _ \    __ _   _ __  | |_  | |__     | |/ /   __ _  | |__    _ __    __ _ 
+		| |_) |  / _` | | '__| | __| | '_ \    | ' /   / _` | | '_ \  | '__|  / _` |
+		|  __/  | (_| | | |    | |_  | | | |   | . \  | (_| | | |_) | | |    | (_| |
+		|_|      \__,_| |_|     \__| |_| |_|   |_|\_\  \__,_| |_.__/  |_|     \__,_|
+
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define str string
+#define mod 1000000007
+void solve(){
+	ll t;cin>>t;
+	while(t--){
+		str s;cin>>s;
+		ll ans=s.length();
+		cout<<ans<<endl;
+	}
+}
+int main(){
+	#ifndef ONLINE_JUDGE
+	    freopen("input.txt", "r", stdin);
+	    freopen("output.txt", "w", stdout);
+	#endif
+	   solve();
+	return 0;
+}
