579a22f974c57bd62d04410088c1ba87c169623f
Parth Santosh Kabra <84372000+parth-kabra@users.noreply.github.com>
2022-06-07 14:24:23 +0530

    Committing files

---
+++ CodeChef/C++14/ANDOR/45806251.cpp
@@ -0,0 +1,27 @@
+/*
+	#Author: Parth Kabra                                                                       
+*/
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define str string
+#define f first
+#define vec vector
+#define TIE cin.tie(NULL);
+const int mod=1e9+7;
+#define IOS ios_base::sync_with_stdio(false);
+void solve(){
+	int t;cin>>t;
+	while(t--){
+		int n;cin>>n;
+		cout<<0<<" "<<n<<endl;
+	}
+}
+int32_t main(){
+	#ifndef ONLINE_JUDGE
+	    freopen("input.txt", "r", stdin);
+	    freopen("output.txt", "w", stdout);
+	#endif
+	   solve();
+	return 0;
+}
