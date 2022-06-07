b8c3c52004741a4241d58508d1eefd4e698f3118
Parth Santosh Kabra <84372000+parth-kabra@users.noreply.github.com>
2022-06-07 15:15:01 +0530

    Committing files

---
+++ CodeForces/GNU C++14/1360D | Buying Shovels/130061767.cpp
@@ -0,0 +1,56 @@
+#include<bits/stdc++.h>
+using namespace std;
+/*-------------------------------------------------------------------------------------------------------------------------------------*/
+#define int long long
+#define endl '\n'
+#define all(s) s.begin(),s.end()
+const int imax=2147483647;
+const int imin=-2147483648;
+const int mod=1e9+7;
+const int N=1e5+10;
+bool prime[N];
+int factorials[N];
+/*-------------------------------------------------------------------------------------------------------------------------------------*/
+void IO(){
+    std::ios_base::sync_with_stdio(false);cin.tie(NULL);
+    #ifndef ONLINE_JUDGE
+        freopen("input.txt", "r", stdin);freopen("uttar.txt", "w", stdout);
+    #endif
+}
+/*-------------------------------------------------------------------------------------------------------------------------------------*/
+void seive(){memset(prime,true,sizeof(prime));for(int p=2;p*p<=N;p++){if (prime[p] == true){for(int j=p*p;j<=N;j+=p){prime[j]=false;}}}}
+void factorial(){factorials[0]=factorials[1]=1;for(int i=2;i<N;i++){factorials[i]=factorials[i-1]*i;}}
+/*-------------------------------------------------------------------------------------------------------------------------------------*/
+ 
+void kabraji_ka_totka(){
+    // totka hai dosto !
+    int n,k;
+    cin>>n>>k;
+    /*for(int i=0;i<k;i++){
+        if(n%i == 0){
+            num = max(num, i);
+        }
+    }*/
+    int res=0;
+    int sq = sqrt(n);
+    for(int i=1;i<=min(k,sq);i++){
+        if(n%i == 0){
+            res = max(res, i);
+            if(n/i <= k){
+                res=max(res, n/i);
+            }
+        }
+    }
+    cout<<((res == 1) ? n : n / res)<<endl;
+}
+ 
+int32_t main(){
+    IO();
+    int t;t=1;
+    cin>>t;
+    while(t--){
+        kabraji_ka_totka();
+    }
+    return 0;
+}
+/*-------------------------------------------------------------------------------------------------------------------------------------*/
