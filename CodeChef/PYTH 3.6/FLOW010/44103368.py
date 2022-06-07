a4cc7f662cb2537b70c536337ee9ce0ed93532c8
Parth Santosh Kabra <84372000+parth-kabra@users.noreply.github.com>
2022-06-07 14:26:33 +0530

    Committing files

---
+++ CodeChef/PYTH 3.6/FLOW010/44103368.py
@@ -0,0 +1,11 @@
+#cook your dish here
+for i in range(int(input())):
+    a=str(input())
+    if (a=="b") or (a=="B"):
+        print("BattleShip")
+    if (a=="c") or (a=="C"):
+        print("Cruiser")
+    if (a=="d") or (a=="D"):
+        print("Destroyer")
+    if (a=="f") or (a=="F"):
+        print("Frigate")
