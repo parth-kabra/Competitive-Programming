d23f8108b89718a53f17cc8f57b917bcd6b51161
Parth Santosh Kabra <84372000+parth-kabra@users.noreply.github.com>
2022-06-07 14:33:08 +0530

    Committing files

---
+++ CodeForces/GNU C++14/505B | Mr. Kitayuta's Colorful Graph/156420805.cpp
@@ -0,0 +1,93 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+ 
+/*
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+template<typename T> using Set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+template<typename T> using Multiset = tree<T, null_type, less_equal<T>, rb_tree_tag, tree_order_statistics_node_update>;
+*/
+ 
+template<class T> bool ckmin(T& a, const T& b) { return b < a ? a = b, 1 : 0; }
+template<class T> bool ckmax(T& a, const T& b) { return a < b ? a = b, 1 : 0; }
+ 
+#define endl '\n'
+#define int long long
+#define debug(x...) cerr<<#x<<" = "<<x<<endl;
+void usaco(string filename = ""){
+    if(filename.size() > 0){
+        #ifndef ONLINE_JUDGE
+            freopen((filename + ".in").c_str(), "r", stdin);
+            freopen((filename + ".out").c_str(), "w", stdout);
+        #endif
+    }
+    else{
+        #ifndef ONLINE_JUDGE
+            freopen("input.txt", "r", stdin);
+            freopen("output.txt", "w", stdout);
+            freopen("error.txt", "w", stderr); 
+        #endif
+    }
+}  
+const int N = 200;
+vector<pair<int,int>> g[N];
+int vis[N];
+bool dfs(int start, int end, int color){
+    vis[start] = true;
+    if(start == end){
+        return true;
+    }
+    for(const pair<int,int> branch : g[start]){
+        if(!vis[branch.first] && branch.second == color){
+            if(dfs(branch.first, end, color)){
+                return true;
+            }
+        }
+    }
+    return false;
+}
+void Solve(){
+    int n, m;
+    cin >> n >> m;
+    while(m--){
+        int u, v, c;
+        cin >> u >> v >> c;
+        u--;
+        v--;
+        c--;
+        g[u].push_back({v, c});
+        g[v].push_back({u, c});
+    }
+    int q;
+    cin >> q;
+    while(q--){
+        int u, v;
+        cin >> u >> v;
+        u--;
+        v--;
+        int res = 0;
+        for(int i = 0; i < 100;i++){
+            memset(vis, 0, sizeof(vis));
+            if(dfs(u, v, i)){
+                res++;
+            }
+        }
+        cout << res << endl;
+    }
+}
+int32_t main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+ 
+    usaco();
+ 
+    int T = 1;
+    //cin >> T;
+    while(T--){
+        Solve();
+    }
+    return 0;
+ 
+}
