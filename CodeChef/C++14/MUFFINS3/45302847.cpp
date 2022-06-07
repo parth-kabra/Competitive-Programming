5a03ebb12516836e34a34707ec8bbf817b53dee6
Parth Santosh Kabra <84372000+parth-kabra@users.noreply.github.com>
2022-06-07 14:25:31 +0530

    Committing files

---
+++ CodeChef/C++14/MUFFINS3/45302847.cpp
@@ -0,0 +1,3 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;int main(){ll t;cin>>t;while (t--){ll s,ans=0;cin>>s;if(s==2){ans=2;}if (s>2){ans=s/2+1;}cout<<ans<<endl;}return 0;}
