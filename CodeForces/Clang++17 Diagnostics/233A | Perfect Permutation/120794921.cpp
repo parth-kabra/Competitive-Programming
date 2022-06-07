813a7ef10f38071b19a721caff0931b8d41d9c65
Parth Santosh Kabra <84372000+parth-kabra@users.noreply.github.com>
2022-06-07 16:00:57 +0530

    Committing files

---
+++ CodeForces/Clang++17 Diagnostics/233A | Perfect Permutation/120794921.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+#include <vector>
+#include <algorithm>
+#include <math.h>
+#include<map>
+#include <cctype>
+#include <unordered_map>
+#include <unordered_set>
+#include <set>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <string>
+using namespace std;
+#define int long long
+const int MAX=1e6;
+void solve(){
+ int n;cin>>n;
+ if(n%2==1){
+  cout<<"-1"<<endl;
+ }
+ else{
+  cout<<"2 1";
+  for(int i=3;i<n;i+=2){
+   cout<<" "<<i+1<<" "<<i;
+  }
+  cout<<endl;
+ }
+}
+int32_t main(){
+ ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+ #ifndef ONLINE_JUDGE
+     freopen("input.txt", "r", stdin);
+     freopen("output.txt", "w", stdout);
+ #endif
+     solve();
+ return 0;
+}
