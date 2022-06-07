9ea7205c8a51dfcff15b148711b8795f49894f37
Parth Santosh Kabra <84372000+parth-kabra@users.noreply.github.com>
2022-06-07 14:24:37 +0530

    Committing files

---
+++ CodeChef/C++14/UWCOI21A/45736528.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define str string
+void solve(){
+	int t;cin>>t;
+	while(t--){
+		int n;cin>>n;
+		cout<<1<<" "<<n<<endl;
+	}
+}
+int32_t main(){
+	#ifndef ONLINE_JUDGE
+	    freopen("input.txt", "r", stdin);
+	    freopen("output.txt", "w", stdout);
+	#endif
+	   solve();
+	return 0;
+}
