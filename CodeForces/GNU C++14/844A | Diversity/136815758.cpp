6cf53af6ae76fa2d6f9e34d9515f7be0b51400ce
Parth Santosh Kabra <84372000+parth-kabra@users.noreply.github.com>
2022-06-07 14:59:33 +0530

    Committing files

---
+++ CodeForces/GNU C++14/844A | Diversity/136815758.cpp
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace std;
+using namespace __gnu_pbds;
+#define int long long
+#define endl '\n'
+#define debug(x...) cerr<<#x<<" = "<<x<<endl;
+//INORDER(L,Root,R)
+//PREORDED(Root,L,R)
+//POSTORDER(L,R,Root)
+template<typename T> using Set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+void usaco(string filename = ""){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    if(filename.size() > 0){
+        #ifndef ONLINE_JUDGE
+            freopen((filename + ".in").c_str(), "r", stdin);
+            freopen((filename + ".out").c_str(), "w", stdout);
+        #endif
+    }
+    else{
+        #ifndef ONLINE_JUDGE
+            freopen("input.txt", "r", stdin);
+            freopen("output.txt", "w", stdout);
+            freopen("error.txt", "w", stderr); 
+        #endif
+    }
+} 
+int32_t main(){
+    usaco();
+ 
+    string s;
+    cin >> s;
+    int n = s.size();
+ 
+    int k;
+    cin >> k;
+    if(k > n){
+        cout << "impossible" << endl; 
+    }
+    else{
+        set<char> tmp(s.begin(), s.end());
+        cout << max(0LL, (int)k-tmp.size()) << endl;
+    }
+    
+    return 0;
+}
