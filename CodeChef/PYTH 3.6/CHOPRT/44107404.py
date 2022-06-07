aa97e57e633098029302f9d4a329d428bd267ce1
Parth Santosh Kabra <84372000+parth-kabra@users.noreply.github.com>
2022-06-07 14:26:29 +0530

    Committing files

---
+++ CodeChef/PYTH 3.6/CHOPRT/44107404.py
@@ -0,0 +1,9 @@
+#cook your dish here
+for i in range(int(input())):
+    a,b=list(map(int,input().split()))
+    if (a>b):
+        print(">")
+    elif (a<b):
+        print("<")
+    else:
+        print("=")
