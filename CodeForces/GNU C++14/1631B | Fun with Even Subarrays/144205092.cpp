713b07684ce72edc6afa5a219c95e4b1e367ad8a
Parth Santosh Kabra <84372000+parth-kabra@users.noreply.github.com>
2022-06-07 14:46:28 +0530

    Committing files

---
+++ CodeForces/GNU C++14/1631B | Fun with Even Subarrays/144205092.cpp
@@ -0,0 +1,46 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define endl '\n'
+#define int long long
+#define debug(x...) cerr<<#x<<" = "<<x<<endl;
+void usaco(string filename = ""){
+    if(filename.size() > 0){
+        #ifndef ONLINE_JUDGE
+            freopen((filename + ".in").c_str(), "r", stdin);
+            freopen((filename + ".out").c_str(), "w", stdout);
+        #endif
+    }
+    else{
+        #ifndef ONLINE_JUDGE
+            freopen("input.txt", "r", stdin);
+            freopen("output.txt", "w", stdout);
+            freopen("error.txt", "w", stderr); 
+        #endif
+    }
+}
+void Solve(){
+    int n;
+    cin >> n;
+    int arr[n];
+    for(int i=0; i < n; i++) cin >> arr[i];
+    int res = 0, k = 1;
+    for(int i= n -2; i >= 0; i--){
+        if(arr[i] == arr[n-1]) k++;
+        else{res++;i -= (k - 1);k <<= 1;}
+    }
+    cout << res << endl;
+}
+int32_t main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+ 
+    usaco();
+ 
+    int t = 1;
+    cin >> t;
+    while(t--){
+        Solve();
+    }
+ 
+    return 0;
+}
