7114821d610560ca8121db0b919cbc29d3bc3fe0
Parth Santosh Kabra <84372000+parth-kabra@users.noreply.github.com>
2022-06-07 15:59:43 +0530

    Committing files

---
+++ CodeForces/GNU C++11/959A | Mahmoud and Ehab and the even-odd game/121295234.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define int long long
+#define endl "\n"
+const int MAX=1e6;
+const int mod=1e9+7;
+void solve(){
+    int n;cin>>n;
+    if(n%2==0){
+        cout<<"Mahmoud"<<endl;
+    }
+    else{
+        cout<<"Ehab"<<endl;
+    }
+}
+int32_t main(){
+    ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+    #ifndef ONLINE_JUDGE
+        freopen("input.txt", "r", stdin);
+        freopen("output.txt", "w", stdout);
+    #endif
+        solve();
+    return 0;
+}
