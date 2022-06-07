696677840b03937a813a3479a2316a52c2fd5b90
Parth Santosh Kabra <84372000+parth-kabra@users.noreply.github.com>
2022-06-07 14:22:49 +0530

    Committing files

---
+++ CodeChef/C++14/BESTEN28/48325414.cpp
@@ -0,0 +1,110 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+//CODE FASTER
+#pragma GCC target ("avx2")
+#pragma GCC optimization ("O3")
+#pragma GCC optimization ("unroint-loops")
+
+//MACROS 
+#define int 					long long
+#define str 					string
+#define ff 						first
+#define vec 					vector
+#define naturalNumSum(N) 		N*(N+1)/2
+const int mod=					1e9+7;
+#define gcd(a,b)				 __gcd(a,b);
+#define lower(string) 			tolower(string)
+#define lcm(a,b) 				a*b/gcd(a,b);
+#define pi 						pair
+#define ss 						second
+#define tc(t) 					while(t--)
+#define pb(a) 					push_back(a);
+#define endl 					"\n"
+#define db 						double
+#define clr(a) 					a.clear();
+#define fl 						float
+#define del 					delete
+#define umap 					unordered_map
+#define ins 					insert
+#define emp(s) 					s.empty()
+#define ppf(v)					v.pop_front();
+#define sortArr(arr,n)			sort(arr,arr+n);
+#define sortVec(arr)			sort(arr.begin(),arr.end())
+#define cin(a) 					cin>>a;
+#define input(v,n) 				for(int i=0;i<n;i++){int m;cin(m);v.pb(m);}
+#define elif 					else if
+#define len(string)				string.length()
+#define print(a) 				cout<<a<<endl;
+#define lb 						lower_bound
+#define ub 						upper_bound
+#define uset 					unordered_set
+#define sz(v) 					v.size()
+
+//STL INPUT & OUTPUT
+#define printStack(ss) 			while(!emp(ss)){cout<<ss.top()<<" ";ss.pop();}cout<<endl;			
+#define printArr(arr,sep)		for(auto &i : arr){cout<<i<<sep;}cout<<endl;
+#define printMap(m)				for(auto &v : m){cout<<v.ff<<" "<<v.ss<<endl;}
+#define matinput(arr,n,m)		for(int i=0;i<n;i++){for(int j=0;j<m;j++){cin>>arr[i][j];}}
+#define matoutput(arr,n,m)		for(int i=0;i<n;i++){for(int j=0;j<m;j++){cout<<arr[i][j]<<" ";}print("\n");}
+
+//LINKEDLIST SNIPPETS
+struct Node {
+    int data;
+    struct Node* next;
+};
+ 
+Node* newNode(int key)
+{
+    Node* temp = new Node;
+    temp->data = key;
+    temp->next = NULL;
+    return temp;
+}
+int linkedlistlen(Node * head){
+	int res=0;
+	while(head != NULL){
+		res++;
+		head=head->next;	
+	}
+	return res;
+}
+//USER CODE
+void solve(){
+    int freq;
+    cin>>freq;
+    while(freq--)
+    {
+        int len,num2;
+        str s;
+        cin>>len>>s>>num2;
+        vector<char>res;
+        for(int i=0;i<len;i++)
+        {
+            if((s[i]-'0')%num2!=0)
+            {
+                res.pb(s[i]);
+            }
+        }
+        sortVec(res);
+        for(int i=0;i<len;i++)
+        {
+            if((s[i]-'0')%num2!=0)
+            {
+                s[i] = res.back();
+                res.pop_back();
+            }
+        }
+        cout<<s<<"\n";
+    }
+}
+int32_t main(){
+	ios_base::sync_with_stdio(false);
+   	cin.tie(NULL);
+	#ifndef ONLINE_JUDGE
+	    freopen("input.txt", "r", stdin);
+	    freopen("output.txt", "w", stdout);
+	#endif
+	    solve();
+	return 0;
+}
