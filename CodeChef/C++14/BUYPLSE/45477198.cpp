80987e65429dbe7983ae3ef9baea27b0bda6ac63
Parth Santosh Kabra <84372000+parth-kabra@users.noreply.github.com>
2022-06-07 14:20:54 +0530

    Committing files

---
+++ CodeChef/C++14/BUYPLSE/45477198.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define str string
+void solve(){
+	int a,b,x,y;
+	cin>>a>>b>>x>>y;
+	cout<<(a*x)+(b*y)<<endl;
+}
+int main()
+{
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+
+#endif
+	solve();
+	return 0;
+}
