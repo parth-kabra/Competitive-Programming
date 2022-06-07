6cfdb20922a954c71649f6e0771a2cf78850e699
Parth Santosh Kabra <84372000+parth-kabra@users.noreply.github.com>
2022-06-07 15:59:22 +0530

    Committing files

---
+++ CodeForces/GNU C++14/581A | Vasya the Hipster/121556122.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define int long long int
+#define endl "\n"
+const int imax=INT_MAX;
+const int imin=INT_MIN;
+const int mod=1e9+7;
+void solve(){
+    int a,b;
+    cin>>a>>b;
+    vector<int> res;
+    cout<<min(a,b)<<" "<<max((a-min(a,b))/2,(b-min(a,b))/2)<<endl;
+}
+int32_t main(){
+    ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+    #ifndef ONLINE_JUDGE
+        freopen("input.txt", "r", stdin);
+        freopen("output.txt", "w", stdout);
+    #endif
+        solve();
+    return 0;
+}
