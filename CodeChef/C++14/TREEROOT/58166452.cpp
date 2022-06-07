131a5bf88d6c4c929fd9547ac539695852d9b06f
Parth Santosh Kabra <84372000+parth-kabra@users.noreply.github.com>
2022-06-07 14:22:04 +0530

    Committing files

---
+++ CodeChef/C++14/TREEROOT/58166452.cpp
@@ -0,0 +1,49 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define endl '\n'
+#define int long long
+#define debug(x...) cerr<<#x<<" = "<<x<<endl;
+void usaco(string filename = ""){
+    if(filename.size() > 0){
+        #ifndef ONLINE_JUDGE
+            freopen((filename + ".in").c_str(), "r", stdin);
+            freopen((filename + ".out").c_str(), "w", stdout);
+        #endif
+    }
+    else{
+        #ifndef ONLINE_JUDGE
+            freopen("input.txt", "r", stdin);
+            freopen("output.txt", "w", stdout);
+            freopen("error.txt", "w", stderr); 
+        #endif
+    }
+}
+bool cmp(const pair<int,int> &a, const pair<int,int> &b){
+    return a.second < b.second && a.first < b.first;
+}
+void Solve(){
+    int n;
+    cin >> n;
+    int res = 0;
+    for(int i=0; i < n;i++){
+        int u,cst;
+        cin >> u >> cst;
+        res += (u - cst);
+        debug(u - cst)
+    }
+    cout << res << endl;
+}
+int32_t main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+ 
+    usaco();
+
+    int t = 1;
+    cin >> t;
+    while(t--){
+        Solve();
+    }
+ 
+    return 0;
+}
