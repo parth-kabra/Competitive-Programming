b19534611707b95456c135141a15939765751ca0
Parth Santosh Kabra <84372000+parth-kabra@users.noreply.github.com>
2022-06-07 14:24:24 +0530

    Committing files

---
+++ CodeChef/PYTH 3.6/GCDMOD/45793578.py
@@ -0,0 +1,8 @@
+import math
+
+M = 10**9+7
+
+for _ in range(int(input())):
+    a, b, n = map(int, input().split())
+    if a == b: print(pow(a, n) * 2 % M)
+    else: print(math.gcd(pow(a, n, a - b) + pow(b, n, a - b), (a - b)) % M)
