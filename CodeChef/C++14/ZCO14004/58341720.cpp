25bd8f5b20c53557a85ab15a2e58cc6940abebe6
Parth Santosh Kabra <84372000+parth-kabra@users.noreply.github.com>
2022-06-07 14:21:11 +0530

    Committing files

---
+++ CodeChef/C++14/ZCO14004/58341720.cpp
@@ -0,0 +1,52 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define endl '\n'
+#define int long long
+#define debug(x...) cerr<<#x<<" = "<<x<<endl;
+void usaco(string filename = ""){
+    if(filename.size() > 0){
+        #ifndef ONLINE_JUDGE
+            freopen((filename + ".in").c_str(), "r", stdin);
+            freopen((filename + ".out").c_str(), "w", stdout);
+        #endif
+    }
+    else{
+        #ifndef ONLINE_JUDGE
+            freopen("input.txt", "r", stdin);
+            freopen("output.txt", "w", stdout);
+            freopen("error.txt", "w", stderr); 
+        #endif
+    }
+}
+void Solve(){
+    int n;
+    cin >> n;
+    int dp[n+5]{};
+    valarray<int> arr(n);
+    for(int &i : arr){
+        cin >> i;
+    }
+    dp[0] = arr[0];
+    dp[1] = arr[1];
+    dp[2] = arr[2];
+    for(int i=3; i < n;i++){
+        dp[i] = arr[i] + min({dp[i-1], dp[i-2], dp[i-3]});
+    }
+    valarray<int> res = {dp[n-1], dp[n-2], dp[n-3]};
+    cout << arr.sum() - res.min() << endl;
+}
+
+int32_t main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+ 
+    usaco();
+
+    int t = 1;
+    //cin >> t;
+    while(t--){
+        Solve();
+    }
+ 
+    return 0;
+}
