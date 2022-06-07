a7ab0ef6f95d69d5a5f7cf90ceaec773bb7d9033
Parth Santosh Kabra <84372000+parth-kabra@users.noreply.github.com>
2022-06-07 14:11:09 +0530

    Committing files

---
+++ Atcoder/Python (3.8.2)/abc237_d/28971032.py
@@ -0,0 +1,50 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define endl '\n'
+#define int long long
+#define debug(x...) cerr<<#x<<" = "<<x<<endl;
+void usaco(string filename = ""){
+    if(filename.size() > 0){
+        #ifndef ONLINE_JUDGE
+            freopen((filename + ".in").c_str(), "r", stdin);
+            freopen((filename + ".out").c_str(), "w", stdout);
+        #endif
+    }
+    else{
+        #ifndef ONLINE_JUDGE
+            freopen("input.txt", "r", stdin);
+            freopen("output.txt", "w", stdout);
+            freopen("error.txt", "w", stderr); 
+        #endif
+    }
+}
+void Solve(){
+    set<int> s;
+    int n;
+    cin >> n;
+    int arr[n];
+    for(int i=0; i < n;i++){
+        cin >> arr[i];
+    }
+    for(int i=0; i <= n;i++){
+        s.insert(i);
+    }
+    for(int i= 0 ; i < n;i++){
+        s.erase(arr[i]);
+        cout << *s.begin() << endl;
+    }
+}
+int32_t main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+ 
+    usaco();
+
+    int t = 1;
+    //cin >> t;
+    while(t--){
+        Solve();
+    }
+ 
+    return 0;
+}
