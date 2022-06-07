8a677f6c577b248a175b6ae2fe56108923aa221e
Parth Santosh Kabra <84372000+parth-kabra@users.noreply.github.com>
2022-06-07 16:01:42 +0530

    Committing files

---
+++ CodeForces/GNU C++14/281A | Word Capitalization/120652218.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+#include <vector>
+#include <algorithm>
+#include <math.h>
+#include<map>
+#include <cctype>
+#include <unordered_map>
+#include <unordered_set>
+#include <set>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <string>
+using namespace std;
+#define int long long
+const int MAX=1e6;
+void solve(){
+ string s;
+ cin>>s;
+ s[0]=toupper(s[0]);
+ cout<<s<<endl;
+}
+int32_t main(){
+ ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+ #ifndef ONLINE_JUDGE
+     freopen("input.txt", "r", stdin);
+     freopen("output.txt", "w", stdout);
+ #endif
+     solve();
+ return 0;
+}
