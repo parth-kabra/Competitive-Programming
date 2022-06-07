eca21c19a99024148a337fdde0a41ebd72c414df
Parth Santosh Kabra <84372000+parth-kabra@users.noreply.github.com>
2022-06-07 16:00:20 +0530

    Committing files

---
+++ CodeForces/GNU C++14/617A | Elephant/120861954.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define int long long
+const int MAX=1e6;
+void solve(){
+  int n;cin>>n;
+  cout<<(n+4)/5<<endl;
+}
+int32_t main(){
+ ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+ #ifndef ONLINE_JUDGE
+     freopen("input.txt", "r", stdin);
+     freopen("output.txt", "w", stdout);
+ #endif
+     solve();
+ return 0;
+}
