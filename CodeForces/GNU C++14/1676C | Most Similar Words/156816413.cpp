1be6020681fd07ba29cfe8800c96584f2b5f0632
Parth Santosh Kabra <84372000+parth-kabra@users.noreply.github.com>
2022-06-07 14:32:00 +0530

    Committing files

---
+++ CodeForces/GNU C++14/1676C | Most Similar Words/156816413.cpp
@@ -0,0 +1,66 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+ 
+/*
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+template<typename T> using Set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+template<typename T> using Multiset = tree<T, null_type, less_equal<T>, rb_tree_tag, tree_order_statistics_node_update>;
+*/
+ 
+template<class T> bool ckmin(T& a, const T& b) { return b < a ? a = b, 1 : 0; }
+template<class T> bool ckmax(T& a, const T& b) { return a < b ? a = b, 1 : 0; }
+ 
+#define endl '\n'
+#define int long long
+#define debug(x...) cerr<<#x<<" = "<<x<<endl;
+void usaco(string filename = ""){
+    if(filename.size() > 0){
+        #ifndef ONLINE_JUDGE
+            freopen((filename + ".in").c_str(), "r", stdin);
+            freopen((filename + ".out").c_str(), "w", stdout);
+        #endif
+    }
+    else{
+        #ifndef ONLINE_JUDGE
+            freopen("input.txt", "r", stdin);
+            freopen("output.txt", "w", stdout);
+            freopen("error.txt", "w", stderr); 
+        #endif
+    }
+}  
+void Solve(){
+    int n, m;
+    cin >> n >> m;
+    int res = 1e18;
+    vector<string>arr(n);
+    for(string &i : arr){
+        cin >> i;
+    }
+    for(int i=0; i < n;i++){
+        for(int j = i + 1; j < n; j++){
+            int cnt = 0;
+            for(int k = 0; k < m ; k++){
+                cnt += abs(arr[i][k] - arr[j][k]);
+            }
+            ckmin(res, cnt);
+        }
+    }
+    cout << res << endl;
+}
+int32_t main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+ 
+    usaco();
+ 
+    int T = 1;
+    cin >> T;
+    while(T--){
+        Solve();
+    }
+    return 0;
+ 
+}
