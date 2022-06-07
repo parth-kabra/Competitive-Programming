d4b8357b77e8f5cdbaf3346da02859cc62fdd2bd
Parth Santosh Kabra <84372000+parth-kabra@users.noreply.github.com>
2022-06-07 14:24:58 +0530

    Committing files

---
+++ CodeChef/C++14/SLOOP/45709529.cpp
@@ -0,0 +1,32 @@
+/*
+		 ____                   _     _         _  __          _                    
+		|  _ \    __ _   _ __  | |_  | |__     | |/ /   __ _  | |__    _ __    __ _ 
+		| |_) |  / _` | | '__| | __| | '_ \    | ' /   / _` | | '_ \  | '__|  / _` |
+		|  __/  | (_| | | |    | |_  | | | |   | . \  | (_| | | |_) | | |    | (_| |
+		|_|      \__,_| |_|     \__| |_| |_|   |_|\_\  \__,_| |_.__/  |_|     \__,_|
+
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define str string
+#define vector vi
+void solve(){
+	int t;cin>>t;
+	while(t--){
+		int m,s;
+		cin>>m>>s;
+		cout<<m/s<<endl;
+	}
+	
+}
+
+int main(){
+	#ifndef ONLINE_JUDGE
+	    freopen("input.txt", "r", stdin);
+	    freopen("output.txt", "w", stdout);
+	#endif
+	   solve();
+	return 0;
+}
