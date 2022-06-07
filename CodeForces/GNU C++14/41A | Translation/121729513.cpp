0060827834a85f1d69f106339b448d088b1062d0
Parth Santosh Kabra <84372000+parth-kabra@users.noreply.github.com>
2022-06-07 15:58:52 +0530

    Committing files

---
+++ CodeForces/GNU C++14/41A | Translation/121729513.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define int long long int
+#define endl "\n"
+const int imax=INT_MAX;
+const int imin=INT_MIN;
+const int mod=1e9+7;
+void solve(){
+    string a,b;
+    cin>>a>>b;
+    reverse(b.begin(),b.end());
+    if(a==b){
+        cout<<"YES"<<endl;
+    }
+    else{
+        cout<<"NO"<<endl;
+    }
+}
+int32_t main(){
+    ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+    #ifndef ONLINE_JUDGE
+        freopen("input.txt", "r", stdin);
+        freopen("output.txt", "w", stdout);
+    #endif
+        solve();
+    return 0;
+}
