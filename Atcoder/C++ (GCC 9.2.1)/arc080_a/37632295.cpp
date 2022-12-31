3b05d2273aa0b13e83811243840ba562e26398ae
Parth Santosh Kabra <84372000+parth-kabra@users.noreply.github.com>
2022-12-31 08:39:31 +0530

    Committing files

---
+++ Atcoder/C++ (GCC 9.2.1)/arc080_a/37632295.cpp
@@ -0,0 +1,91 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+ 
+/*
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+template<typename T> using Set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+template<typename T> using Multiset = tree<T, null_type, less_equal<T>, rb_tree_tag, tree_order_statistics_node_update>;
+*/
+ 
+template<class T> bool ckmin(T& a, const T& b) { return b < a ? a = b, 1 : 0; }
+template<class T> bool ckmax(T& a, const T& b) { return a < b ? a = b, 1 : 0; }
+ 
+#define endl '\n'
+#define int long long
+#define debug(x...) cerr<<#x<<" = "<<x<<endl;
+void usaco(string filename = ""){
+    if(filename.size() > 0){
+        #ifndef ONLINE_JUDGE
+            freopen((filename + ".in").c_str(), "r", stdin);
+            freopen((filename + ".out").c_str(), "w", stdout);
+        #endif
+    }
+    else{
+        #ifndef ONLINE_JUDGE
+            freopen("input.txt", "r", stdin);
+            freopen("output.txt", "w", stdout);
+            freopen("error.txt", "w", stderr); 
+        #endif
+    }
+}  
+void Solve(){
+    int n;
+    cin >> n;
+    vector<int> arr(n);
+    int one = 0;
+    int two = 0;
+    int four = 0;
+    for(int &i : arr){
+        cin >> i;
+        if(i & 1) one++;
+        else if(i % 4 == 0) four++;
+        else if(i % 2 == 0) two++;
+    }
+    bool found = true;
+    for(int i =0; i < n - 1; i++){
+        if((arr[i] * arr[i + 1]) % 4 != 0){
+            found =false;
+            break;
+        }
+    }
+    if(found){
+        cout << "Yes" << endl;
+        return;
+    }
+    debug(one)
+    debug(two)
+    debug(four)
+    if(!two){
+        if(one <= four + 1){
+            cout << "Yes" << endl; 
+        }
+        else{
+            cout << "No" << endl;
+        }
+    }
+    else{
+        if(one <= four){
+            cout << "Yes" << endl;
+        }
+        else{
+            cout << "No" << endl;
+        }
+    }
+}
+int32_t main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+ 
+    usaco();
+ 
+    int T = 1;
+    //cin >> T;
+    while(T--){
+        Solve();
+    }
+    return 0;
+ 
+}
