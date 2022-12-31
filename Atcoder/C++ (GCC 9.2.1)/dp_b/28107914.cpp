d7b230563362ec76c5746c121651f7431b42909e
Parth Santosh Kabra <84372000+parth-kabra@users.noreply.github.com>
2022-12-31 08:41:39 +0530

    Committing files

---
+++ Atcoder/C++ (GCC 9.2.1)/dp_b/28107914.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define debug(x...) cerr<<#x<<" = "<<x<<endl;
+void usaco(string filename = ""){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    if(filename.size() > 0){
+        #ifndef ONLINE_JUDGE
+            freopen((filename + ".in").c_str(), "r", stdin);
+            freopen((filename + ".out").c_str(), "w", stdout);
+        #endif
+    }
+    else{
+        #ifndef ONLINE_JUDGE
+            freopen("input.txt", "r", stdin);
+            freopen("output.txt", "w", stdout);
+            freopen("error.txt", "w", stderr); 
+        #endif
+    }
+}
+const int N = 1e5+5;
+int dp[N];
+int arr[N];
+int32_t main(){
+    usaco();
+
+    int n,k;
+    cin >> n >> k;
+    for(int i=1; i <=n ;i++){
+        cin >> arr[i];
+        dp[i] = 1e9;
+    }
+    dp[1] = 0;
+    for(int i=1; i <= n ;i++){
+        for(int j=1; j <= k;j++){
+            if(i + j <= n){
+                dp[i+j] = min(dp[i+j],dp[i] + abs(arr[i] - arr[i+j]));
+            }
+        }
+    }
+    cout << dp[n] << endl;
+
+    return 0;
+}
