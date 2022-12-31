49ba17ee1d9cbd3e073657ec459f9468bb834dfe
Parth Santosh Kabra <84372000+parth-kabra@users.noreply.github.com>
2022-12-31 08:39:46 +0530

    Committing files

---
+++ Atcoder/C++ (GCC 9.2.1)/abc186_a/35992438.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define debug(x...) cerr<<#x<<" = "<<x<<endl;
+void usaco(string filename = ""){
+	ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    if(filename.size() > 0){
+        #ifndef ONLINE_JUDGE
+            freopen((filename + ".in").c_str(), "r", stdin);
+            freopen((filename + ".out").c_str(), "w", stdout);
+        #endif
+    }
+    else{
+        #ifndef ONLINE_JUDGE
+            freopen("input.txt", "r", stdin);
+            freopen("output.txt", "w", stdout);
+            freopen("error.txt", "w", stderr); 
+        #endif
+    }
+} 	
+
+int main(){
+
+	usaco();
+	
+    int n,w;
+    cin >> n >> w;
+ 
+    if ( n >= w)
+        cout << n  / w << endl;
+ 
+    else 
+        cout << "0"<< endl;
+
+}
