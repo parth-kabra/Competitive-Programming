d99647d41268dd754cd130121fe9b25f1885a9ed
Parth Santosh Kabra <84372000+parth-kabra@users.noreply.github.com>
2022-12-31 08:43:09 +0530

    Committing files

---
+++ Atcoder/C++ (GCC 9.2.1)/abc215_b/25220867.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define int long long
+const int imax=(1LL<<31)-1;
+const int imin=INT_MIN;
+const int mod=1e9+7;
+const int N=2e5+10;
+#define LOG2(X) ((unsigned) (8*sizeof (unsigned long long) - __builtin_clzll((X)) - 1))
+void kabraji_ka_totka(){
+        int n;
+        cin>>n;
+        //ll ans =((value10(n))/value10(2));
+        cout<<LOG2(n);
+}
+int32_t main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    #ifndef ONLINE_JUDGE
+        freopen("input.txt", "r", stdin);
+        freopen("result.txt", "w", stdout);
+    #endif
+        kabraji_ka_totka();
+    return 0;
+}
