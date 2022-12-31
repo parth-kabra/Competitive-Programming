c3c7084c93c768f325b04126731927465ac91f4b
Parth Santosh Kabra <84372000+parth-kabra@users.noreply.github.com>
2022-12-31 08:44:07 +0530

    Committing files

---
+++ Atcoder/C++ (GCC 9.2.1)/abc202_a/22860710.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+#pragma GCC target ("avx2")
+#pragma GCC optimization ("O3")
+#pragma GCC optimization ("unroll-loops")
+using namespace std;
+#define int long long
+void solve(){
+	int a,b,c;cin>>a>>b>>c;
+	int res=a+b+c;
+	cout<<21-res<<endl;
+}
+int32_t main(){
+	ios_base::sync_with_stdio(false);
+   	cin.tie(NULL);
+	#ifndef ONLINE_JUDGE
+	    freopen("input.txt", "r", stdin);
+	    freopen("output.txt", "w", stdout);
+	#endif
+	   solve();
+	return 0;
+}
