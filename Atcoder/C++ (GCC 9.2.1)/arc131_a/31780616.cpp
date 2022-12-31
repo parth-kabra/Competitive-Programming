db6f13a6e1e94dc16af2b106966a26078aba7c4d
Parth Santosh Kabra <84372000+parth-kabra@users.noreply.github.com>
2022-12-31 08:40:04 +0530

    Committing files

---
+++ Atcoder/C++ (GCC 9.2.1)/arc131_a/31780616.cpp
@@ -0,0 +1,58 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+/*
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+template<typename T> using Set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+template<typename T> using Multiset = tree<T, null_type, less_equal<T>, rb_tree_tag, tree_order_statistics_node_update>;
+*/
+
+template<class T> bool ckmin(T& a, const T& b) { return b < a ? a = b, 1 : 0; }
+template<class T> bool ckmax(T& a, const T& b) { return a < b ? a = b, 1 : 0; }
+
+#define endl '\n'
+#define int long long
+#define debug(x...) cerr<<#x<<" = "<<x<<endl;
+void usaco(string filename = ""){
+    if(filename.size() > 0){
+        #ifndef ONLINE_JUDGE
+            freopen((filename + ".in").c_str(), "r", stdin);
+            freopen((filename + ".out").c_str(), "w", stdout);
+        #endif
+    }
+    else{
+        #ifndef ONLINE_JUDGE
+            freopen("input.txt", "r", stdin);
+            freopen("output.txt", "w", stdout);
+            freopen("error.txt", "w", stderr); 
+        #endif
+    }
+}  
+bool f(const int &s, const int &a){
+    if(to_string(s).find(to_string(a)) != string::npos){
+        return true;
+    }
+    return false;
+}
+void Solve(){
+    int a, b;
+    cin >> a >> b;
+    cout << (500000000 * b + a) << endl;
+}
+int32_t main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+ 
+    usaco();
+
+    int T = 1;
+    //cin >> T;
+    while(T--){
+        Solve();
+    }
+    return 0;
+
+}
