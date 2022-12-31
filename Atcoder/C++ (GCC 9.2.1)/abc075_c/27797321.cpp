ff5eb14cee6f1f36caabff58c5585592393333c5
Parth Santosh Kabra <84372000+parth-kabra@users.noreply.github.com>
2022-12-31 08:42:10 +0530

    Committing files

---
+++ Atcoder/C++ (GCC 9.2.1)/abc075_c/27797321.cpp
@@ -0,0 +1,88 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define endl '\n'
+#define debug(x...) cerr<<#x<<" = "<<x<<endl;
+void usaco(string filename = ""){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    if(filename.size() > 0){
+        #ifndef ONLINE_JUDGE
+            freopen((filename + ".in").c_str(), "r", stdin);
+            freopen((filename + ".out").c_str(), "w", stdout);
+        #endif
+    }
+    else{
+        #ifndef ONLINE_JUDGE
+            freopen("input.txt", "r", stdin);
+            freopen("output.txt", "w", stdout);
+            freopen("error.txt", "w", stderr); 
+        #endif
+    }
+}
+// ds here...
+template<class T> class UFDS{
+    private:
+    vector<T> par,siz;
+ 
+    public:
+    UFDS(T n) : par(n,-1),siz(n,1){}
+    T root (T x){
+        if (par[x] == -1) return x;
+        else return par[x] = root(par[x]);
+    }
+    bool same(T x,T y){
+        return root(x) == root(y);
+    }
+    bool unite(T x,T y){
+        x = root(x);y=root(y);  
+        if (x==y) return false; 
+        if (siz[x] < siz[y]) swap(x,y);
+        par[y] = x;
+        siz[x] += siz[y]; 
+        return true;
+    }
+    T size(T x){
+        return siz[root(x)];
+    }
+    void init(){
+        for(auto& x:par) x = -1;
+        for(auto& x:siz) x = 1;
+    }
+};
+int32_t main(){
+    usaco();
+
+    // code here...
+    int n,m;
+    cin >> n >> m;
+    int u[m], v[m];
+    for(int i=0; i < m ;i++){
+        cin >> u[i] >> v[i];
+        u[i]--;
+        v[i]--;
+    }
+    int res =0;
+    UFDS<int> ufds(n);
+    for(int i=0; i < m;i++){
+        ufds.init();
+        for(int j =0;j < m ;j++){
+            if(i!=j){
+                ufds.unite(u[j], v[j]);
+            }
+        }
+        int members = 0;
+        for(int i=0; i < n ;i++){
+            if(i == ufds.root(i)){
+                members++;
+            }
+        }
+        if(members > 1){
+            res++;
+        }
+    }
+    cout << res << endl;
+
+    return 0;
+}
