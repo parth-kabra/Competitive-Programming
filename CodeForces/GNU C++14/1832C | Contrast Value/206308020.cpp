dbc1b0fcb029b107c429d6cc5823d6cfb8b993cd
Parth Santosh Kabra <84372000+parth-kabra@users.noreply.github.com>
2023-06-07 14:06:49 +0530

    Committing files

---
+++ CodeForces/GNU C++14/1832C | Contrast Value/206308020.cpp
@@ -0,0 +1,70 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+ 
+/*
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+template<typename T> using Set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+template<typename T> using Multiset = tree<T, null_type, less_equal<T>, rb_tree_tag, tree_order_statistics_node_update>;
+*/
+ 
+template<class T> bool ckmin(T& a, const T& b) { return b < a ? a = b, 1 : 0; }
+template<class T> bool ckmax(T& a, const T& b) { return a < b ? a = b, 1 : 0; }
+ 
+#define endl '\n'
+#define int long long
+#define debug(x...) cerr<<#x<<" = "<<x<<endl;
+void usaco(string filename = ""){
+    if(filename.size() > 0){
+        #ifndef ONLINE_JUDGE
+            freopen((filename + ".in").c_str(), "r", stdin);
+            freopen((filename + ".out").c_str(), "w", stdout);
+        #endif
+    }
+    else{
+        #ifndef ONLINE_JUDGE
+            freopen("input.txt", "r", stdin);
+            freopen("output.txt", "w", stdout);
+            freopen("error.txt", "w", stderr); 
+        #endif
+    }
+}
+// om hanumante namah  
+void Solve(){
+    int n;
+    cin >> n;
+    vector<int> arr;
+    while(n--){
+        int x;
+        cin >> x;
+        arr.push_back(x);
+    }
+    n = unique(arr.begin(), arr.end()) - arr.begin();
+    int res = n ;
+    for(int i =0; i+ 2 < n ;i++){
+        if(arr[i] > arr[i + 1] && arr[i + 1] > arr[i + 2]){
+            res --;
+        }
+        if(arr[i] < arr[i + 1] && arr[i + 1] < arr[i + 2]){
+            res --;
+        }
+    }
+    debug(res)
+    cout << res << endl;
+}
+int32_t main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+ 
+    usaco();
+ 
+    int T = 1;
+    cin >> T;
+    while(T--){
+        Solve();
+    }
+    return 0;
+ 
+}
